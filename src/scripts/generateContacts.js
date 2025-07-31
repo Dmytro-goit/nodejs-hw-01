18abd9926a13a242c7caf2905051ea4c983e9a40
Dmytro-goit <dmytro.moiyseev@gmail.com>
2025-07-31 13:35:48 +0300

    -

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,16 @@
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+
+  const newContacts = Array.from({ length: number }, () => createFakeContact());
+
+  const updatedContacts = [...contacts, ...newContacts];
+
+  await writeContacts(updatedContacts);
+  console.log(`✅ Успішно згенеровано ${number} контакт(ів).`);
+};
+
+generateContacts(5);
---
+++ src/utils/writeContacts.js
@@ -0,0 +1,7 @@
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts);
+  await fs.writeFile(PATH_DB, data, 'utf-8');
+};
